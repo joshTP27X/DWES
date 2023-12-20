228443f3155ad4100ce614cec51958d04b8db44c
Joseph Arizpe Colmenares <josaricol@alu.edu.gva.es>
2023-12-20 17:39:07 +0100

    practica7 - parte3, pag21

---
+++ MvcSoporte/MvcSoporte/Models/Localizacion.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcSoporte.Models
+{
+    public class Localizacion
+    {
+        public int Id { get; set; }
+        [Display(Name = "Descripción")]
+        [Required(ErrorMessage = "La descripción de la localización es un campo requerido.")]
+        public string? Descripcion { get; set; }
+        public ICollection<Equipo>? Equipos { get; set; }
+    }
+}
